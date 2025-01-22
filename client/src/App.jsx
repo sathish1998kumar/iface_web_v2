34ae781c0911825da689dc56bd0c27387591d1ff
jayaprakashSs <jayaprakash.domain@gmail.com>
2025-01-22 17:42:16 +0530

    Add 404 page not found JSX component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Reports from "./pages/Reports/Reports";
-import DailyReport from "./pages/Reports/DailyReport"; // Import the DailyReport component
-import Layout from "./components/Layout"; 
+import DailyReport from "./pages/Reports/DailyReport";
+import Layout from "./components/Layout";
+import AttendanceDashboard from "./pages/AttendanceDashboard"; // Import the AttendanceDashboard component
+import NotFound from "./pages/NotFound"; // Import the NotFound (404 Page) component
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
@@ -20,8 +22,13 @@ const App = () => (
       <Route path="/reports" element={<Reports />} />
 
       {/* Route for Daily Report page */}
-      <Route path="/reports/daily"  element={ <Layout><DailyReport /></Layout>}
-      />
+      <Route path="/reports/daily" element={<Layout><DailyReport /></Layout>} />
+
+      {/* Route for AttendanceDashboard after login */}
+      <Route path="/attendance-dashboard" element={<AttendanceDashboard />} />
+
+      {/* 404 Page Route */}
+      <Route path="*" element={<NotFound />} />
     </Routes>
   </Router>
 );
