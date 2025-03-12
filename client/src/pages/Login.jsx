7536570eb68ff45539e4ef0c7b571b5753e5a69e
jayaprakashSs <jayaprakash.domain@gmail.com>
2025-03-12 12:42:17 +0530

    Fixed login issue and updated UI - 12th March 2025

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -20,13 +20,10 @@ const schema = z.object({
 const FloatingPaths = ({ position }) => {
   const paths = Array.from({ length: 36 }, (_, i) => ({
     id: i,
-    d: `M-${380 - i * 5 * position} -${189 + i * 6}C-${
-      380 - i * 5 * position
-    } -${189 + i * 6} -${312 - i * 5 * position} ${216 - i * 6} ${
-      152 - i * 5 * position
-    } ${343 - i * 6}C${616 - i * 5 * position} ${470 - i * 6} ${
-      684 - i * 5 * position
-    } ${875 - i * 6} ${684 - i * 5 * position} ${875 - i * 6}`,
+    d: `M-${380 - i * 5 * position} -${189 + i * 6}C-${380 - i * 5 * position
+      } -${189 + i * 6} -${312 - i * 5 * position} ${216 - i * 6} ${152 - i * 5 * position
+      } ${343 - i * 6}C${616 - i * 5 * position} ${470 - i * 6} ${684 - i * 5 * position
+      } ${875 - i * 6} ${684 - i * 5 * position} ${875 - i * 6}`,
     color: `rgba(0, 0, 0, ${0.1 + i * 0.03})`, // Black color with varying opacity
     width: 0.5 + i * 0.03,
   }));
